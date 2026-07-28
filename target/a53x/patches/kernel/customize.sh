055b15fa83a7eabc58a982a3add6aa2cd8202071
Majaahh <maja.blaszczak@proton.me>
2026-07-28 10:49:38 +0200

    a53x: kernel: return when kernel/DTBO archive URL is empty
    Change-Id: If874ff9c9bd08b74ce8a426c25b81ebd31c9bce1
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- target/a53x/patches/kernel/customize.sh
+++ target/a53x/patches/kernel/customize.sh
@@ -12,6 +12,16 @@ GET_URL()
 KERNEL_ARCHIVE_URL="$(GET_URL "^UN1CA_Kernel-.*-a53x\.tar$")"
 DTBO_ARCHIVE_URL="$(GET_URL "^UN1CA_DTBO-.*-a53x\.tar$")"
 
+if [ -z "$KERNEL_ARCHIVE_URL" ]; then
+    LOGE "Failed to fetch kernel archive URL"
+    return 1
+fi
+
+if [ -z "$DTBO_ARCHIVE_URL" ]; then
+    LOGE "Failed to fetch DTBO archive URL"
+    return 1
+fi
+
 if [ -d "$TMP_DIR" ]; then
     EVAL "rm -rf \"$TMP_DIR\""
 fi
